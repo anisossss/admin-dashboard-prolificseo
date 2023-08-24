ae5f2b2147dbe4f66406118ca5fdc310e479ff08
anis khalef <anis.khalef@duetodata.com>
2023-08-24 10:38:24 +0100

    changes

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
 export const CONSTANTS = {
-  // API_URL_PROD: "http://localhost:4401/api",
-  API_URL_PROD: "https://back.prolificseo.com/api",
+  API_URL_PROD: "http://localhost:4401/api",
+  // API_URL_PROD: "https://back.prolificseo.com/api",
 };
